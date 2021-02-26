0430ac43972786d52186922cac388841368db757
Sarkash Talib <sarkash.talib@gmail.com>
2021-02-26 16:35:47 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,29 @@
+# Set items are enclosed in curly brackets
+# Set is unordered
+# Set is mutable, can contain only immutable items
+# Set elements are unique
+# Elements can contain only immutable items
+
+
+"""
+Sets can be used for mathematical set operations such as union, intersection, difference 
+and symmetric difference.
+
+https://en.wikipedia.org/wiki/Set_(mathematics)
+"""
+
+'''
+Python set Attributes
+'''
+
+# print(dir(set))
+
+'''
+Creating Python sets
+'''
+
+# set = {} #this is not a set intead it's a dictionary.
+# set = set()
+# set = {1,2,3}
+# print(type(set))
+# print(set)
